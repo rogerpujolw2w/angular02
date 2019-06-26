7c7d6d5f4ac78461166630c33ce08b4a88adef8c
unknown <roger-pujol@with2webs.com>
2019-06-26 12:38:09 +0200

    update

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -9,10 +9,35 @@ import * as moment from 'moment';
 export class MainHeaderComponent implements OnInit {
 
   fecha:string;
+  disBoton1:boolean = false;
+  disBoton2:boolean = true;
+  disBoton3:boolean = true;
+  clase: string = "btn btn-primary";
+  clase2: string = "btn btn-primary";
+  clase3: string = "btn btn-primary";
+  txtBtn: string = "Botón verde";
+  txtBtn2: string = "Botón azul";
+  txtBtn3: string = "Botón rojo";
+
+clickAzul() {
+  alert('Has pulsado el botón');
+}
+
+clickVerde() {
+  this.disBoton1 = true;
+  this.disBoton2 = false;
+  this.disBoton3 = false;
+}
+
+clickRojo() {
+  this.disBoton1 = false;
+  this.disBoton2 = true;
+  this.disBoton3 = true;
+}
 
 constructor() {
   moment.locale('es');
-  this.fecha=moment().format("LLL");
+  this.fecha = moment().format("LLL");
  }
 
 
